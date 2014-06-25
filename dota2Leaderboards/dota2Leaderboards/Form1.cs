a6c259f401a11ee599dcfa0edaf3bc686fd97d64
LanQ <flfmfn_1314@163.com>
2014-06-25 17:06:24 +0800

    增加button

--- dota2Leaderboards/dota2Leaderboards/Form1.cs
+++ dota2Leaderboards/dota2Leaderboards/Form1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace dota2Leaderboards
 {
@@ -15,6 +20,30 @@ namespace dota2Leaderboards
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.dota2.com/webapi/ILeaderboard/GetDivisionLeaderboard/v0001?division=china";
+            HttpWebRequest hwrqP = (HttpWebRequest)WebRequest.Create(url);
+            //hwrqP.Method = "POST";
+
+            HttpWebResponse response = (HttpWebResponse)hwrqP.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string content = reader.ReadToEnd();
+
+            JObject jo = (JObject)JsonConvert.DeserializeObject(content);
+            JArray ja = (JArray)jo["leaderboard"];
+
+            string text = "";
+
+            foreach(JObject o in ja)
+            {
+                text += o["rank"] + "、" + o["team_tag"] + "." + o["name"] + "\n";
+            }
+
+            MessageBox.Show(text);
+
         }
     }
 }
